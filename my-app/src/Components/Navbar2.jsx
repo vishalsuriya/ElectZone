58517dd431d1d291a14113a528d3a0e6b2cd82f4
sivabalan2211 <sivab2211@gmail.com>
2024-01-25 22:18:50 +0530

    align

--- my-app/src/Components/Navbar2.jsx
+++ my-app/src/Components/Navbar2.jsx
@@ -1,25 +1,41 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { IoSearch } from 'react-icons/io5';
-import { FaBars } from 'react-icons/fa';
-import { BsCartCheckFill } from 'react-icons/bs';
-import '../Components/NavbarStyle.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { IoSearch } from "react-icons/io5";
+import { FaBars } from "react-icons/fa";
+import { BsCartCheckFill } from "react-icons/bs";
+import "../Components/NavbarStyle.css";
 function Navbar2() {
   const [showMenu, setShowMenu] = useState(false);
   const handleMenuClick = () => {
     setShowMenu(!showMenu);
   };
   return (
-    <div className='header'>
-        <h1>ElectZone</h1>
+    <div className="header">
+      <h1>ElectZone</h1>
       <ul>
-        <li><input type='text' placeholder='Search' id='searchInput' name='searchInput' /></li>
-        <li><IoSearch /></li>
         <li>
-        <Link to={"/"}> <button>Logout</button></Link> 
+          <input
+            type="text"
+            placeholder="Search"
+            id="searchInput"
+            name="searchInput"
+          />
         </li>
-        <li><BsCartCheckFill /></li>
-        <li onClick={handleMenuClick}><FaBars></FaBars></li>
+        <li>
+          <IoSearch />
+        </li>
+        <li>
+          <Link to={"/"}>
+            {" "}
+            <button className="head-btn">Logout</button>
+          </Link>
+        </li>
+        <li>
+          <BsCartCheckFill />
+        </li>
+        <li onClick={handleMenuClick}>
+          <FaBars></FaBars>
+        </li>
       </ul>
       {showMenu && (
         <div className="menu">
@@ -27,7 +43,9 @@ function Navbar2() {
             <li>My Profile</li>
             <li>My Cart</li>
             <li>Settings</li>
-           <Link to={"/"}><li>Logout</li></Link> 
+            <Link to={"/"}>
+              <li>Logout</li>
+            </Link>
           </ul>
         </div>
       )}
