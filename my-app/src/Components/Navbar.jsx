bc36e5559f2f5ef07e52f0b05aedbe04c0629601
sivabalan2211 <sivab2211@gmail.com>
2024-01-25 21:08:41 +0530

    Login Page

--- my-app/src/Components/Navbar.jsx
+++ my-app/src/Components/Navbar.jsx
@@ -1,3 +1,68 @@
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import { IoSearch } from 'react-icons/io5';
+// import '../Components/NavbarStyle.css';
+
+// function Navbar() {
+//   const [showLoginForm, setShowLoginForm] = useState(false);
+
+//   const handleLoginClick = () => {
+//     setShowLoginForm(true);
+//   };
+
+//   const handleLoginFormClose = () => {
+//     setShowLoginForm(false);
+//   };
+
+//   const handleLoginSubmit = (event) => {
+//     event.preventDefault();
+//     console.log('User logged in!');
+//     setShowLoginForm(false);
+//   };
+
+//   return (
+//     <div>
+//       <div className='header'>
+//         <Link to="/">
+//           <h1>ElectZone</h1>
+//         </Link>
+//         <ul>
+//           <li>
+//             <input type='text' placeholder='Search' id='searchInput' name='searchInput' />
+//           </li>
+//           <li>
+//             <IoSearch />
+//           </li>
+//           <li>
+//             <button onClick={handleLoginClick}>Login</button>
+//           </li>
+//         </ul>
+//       </div>
+
+//       {showLoginForm ? (
+//         <div className="login-form-container">
+//           <form onSubmit={handleLoginSubmit}>
+//             <label>
+//               Username:
+//               <input type="text" autoComplete='off' name="username" />
+//             </label>
+//             <label>
+//               Password:
+//               <input type="password" name="password" />
+//             </label>
+//             <Link to={"/Login"}>
+//               <button type="submit">Submit</button>
+//             </Link>
+//             <button type="button" onClick={handleLoginFormClose}>Close</button>
+//           </form>
+//         </div>
+//       ) : null}
+//     </div>
+//   );
+// }
+
+// export default Navbar;
+
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { IoSearch } from 'react-icons/io5';
@@ -39,24 +104,26 @@ function Navbar() {
         </ul>
       </div>
 
-      {showLoginForm ? (
-        <div className="login-form-container">
-          <form onSubmit={handleLoginSubmit}>
-            <label>
-              Username:
-              <input type="text" autoComplete='off' name="username" />
-            </label>
-            <label>
-              Password:
-              <input type="password" name="password" />
-            </label>
-            <Link to={"/Login"}>
-              <button type="submit">Submit</button>
-            </Link>
-            <button type="button" onClick={handleLoginFormClose}>Close</button>
-          </form>
+      {showLoginForm && (
+        <div className="modal">
+          <div className="modal-content">
+            <form onSubmit={handleLoginSubmit}>
+              <label>
+                Username:
+                <input type="text" autoComplete='off' name="username" />
+              </label>
+              <label>
+                Password:
+                <input type="password" name="password" />
+              </label>
+              <Link to={"/Login"}>
+                <button type="submit">Submit</button>
+              </Link>
+              <button type="button" onClick={handleLoginFormClose}>Close</button>
+            </form>
+          </div>
         </div>
-      ) : null}
+      )}
     </div>
   );
 }
