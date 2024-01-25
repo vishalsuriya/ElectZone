c83269e220b2a6f7c976068ebcbb6b5e6b6fe922
sivabalan2211 <sivab2211@gmail.com>
2024-01-25 21:01:47 +0530

    Login Changes

--- my-app/src/Components/Navbar.jsx
+++ my-app/src/Components/Navbar.jsx
@@ -2,14 +2,18 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { IoSearch } from 'react-icons/io5';
 import '../Components/NavbarStyle.css';
+
 function Navbar() {
   const [showLoginForm, setShowLoginForm] = useState(false);
+
   const handleLoginClick = () => {
     setShowLoginForm(true);
   };
+
   const handleLoginFormClose = () => {
     setShowLoginForm(false);
   };
+
   const handleLoginSubmit = (event) => {
     event.preventDefault();
     console.log('User logged in!');
@@ -17,40 +21,42 @@ function Navbar() {
   };
 
   return (
-    <div className='header'>
-      <Link to="/">
-        <h1>ElectZone</h1>
-      </Link>
-      <ul>
-        <li>
-          <input type='text' placeholder='Search' id='searchInput' name='searchInput' />
-        </li>
-        <li>
-          <IoSearch />
-        </li>
-        {showLoginForm ? (
+    <div>
+      <div className='header'>
+        <Link to="/">
+          <h1>ElectZone</h1>
+        </Link>
+        <ul>
           <li>
-            <div className="login-form-container">
-              <form onSubmit={handleLoginSubmit}>
-                <label>
-                  Username:
-                  <input type="text" autoComplete='off' name="username" />
-                </label>
-                <label>
-                  Password:
-                  <input type="password" name="password" />
-                </label>
-              <Link to={"/Login"}> <button type="submit">Submit</button></Link> 
-                <button type="button" onClick={handleLoginFormClose}>Close</button>
-              </form>
-            </div>
+            <input type='text' placeholder='Search' id='searchInput' name='searchInput' />
           </li>
-        ) : (
+          <li>
+            <IoSearch />
+          </li>
           <li>
             <button onClick={handleLoginClick}>Login</button>
           </li>
-        )}
-      </ul>
+        </ul>
+      </div>
+
+      {showLoginForm ? (
+        <div className="login-form-container">
+          <form onSubmit={handleLoginSubmit}>
+            <label>
+              Username:
+              <input type="text" autoComplete='off' name="username" />
+            </label>
+            <label>
+              Password:
+              <input type="password" name="password" />
+            </label>
+            <Link to={"/Login"}>
+              <button type="submit">Submit</button>
+            </Link>
+            <button type="button" onClick={handleLoginFormClose}>Close</button>
+          </form>
+        </div>
+      ) : null}
     </div>
   );
 }
