aca99d56acafe1e95eb44bf421b2d00f77b378f8
sivabalan2211 <sivab2211@gmail.com>
2024-02-19 11:40:12 +0530

    server.jx

--- Backend/server.js
+++ Backend/server.js
@@ -5,25 +5,6 @@ const connectDB = require("../Backend/Database/connection");
 const Cards = require("../Backend/model/CardModel");
 
 // Use body-parser middleware to parse incoming request bodies
-app.use(express.json());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Allow specific HTTP methods
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow specific headers
-  next();
-});
-connectDB();
-
-app.get("/", async (req, res) => {
-  try {
-    const Card = await Cards.find();
-    // console.log(Card);
-    res.send(Card);
-  } catch (error) {
-    console.error("Error retrieving data:", error);
-    res.status(500).send("Internal Server Error");
-  }
-});
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
