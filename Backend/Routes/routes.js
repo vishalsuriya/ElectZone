e14ca322060384b28701fbeb8fd370e3fff60ff0
vishalsuriya <vishalsuriya2003@gmail.com>
2024-02-19 21:36:25 +0530

    empty

--- Backend/Routes/routes.js
+++
@@ -1,4 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const services = require("../services/render");
-const controller = require("../Controller/controller");
--- Backend/services/render.js
+++
@@ -1,4 +0,0 @@
-//fucntions like viewpatients from these files like clikc view render to view page
-
-const axios = require("axios");
-const { response } = require("express");
