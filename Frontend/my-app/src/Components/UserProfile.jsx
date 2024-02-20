0076e98a7e31e25f07541a9cfbb66be62cbd64d8
vishalsuriya <vishalsuriya2003@gmail.com>
2024-02-20 15:54:35 +0530

    UserProfile created

--- Frontend/my-app/src/Components/UserProfile.jsx
+++ Frontend/my-app/src/Components/UserProfile.jsx
@@ -1,11 +1,78 @@
-import React,{Link} from 'react'
+import React from 'react';
+import { Row, Col, Button, Form } from 'react-bootstrap';
+import "../Components/UserProfile.css";
+import pic from "../assets/Userimage.png";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function UserProfile ()  {
+function UserProfile() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
   return (
     <div>
-    <h1>hello</h1>
-</div>
-  )
+      <Row className="profileContainer">
+        <Col md={6}>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Name"
+                aria-label="Name"
+              />
+            </Form.Group>
+            <Form.Group controlId="email">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter Email"
+                aria-label="Email Address"
+              />
+            </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter Password"
+                aria-label="Password"
+              />
+            </Form.Group>
+            <Form.Group controlId="confirmPassword">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Confirm Password"
+                aria-label="Confirm Password"
+              />
+            </Form.Group>{" "}
+            <Form.Group controlId="pic">
+         <Form.Label>Change Profile Picture</Form.Label>
+         <Form.Control
+            type="file"
+             id="custom-file"
+              label="Upload Profile Picture"
+            custom
+              />
+             </Form.Group>
+            <Button type="submit" variant="primary">
+              Update
+            </Button>
+          </Form>
+        </Col>
+        <Col
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          
+        >
+          <img src={pic}  className="profilePic" />
+          {/* Add content for the second column if needed */}
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default UserProfile;
